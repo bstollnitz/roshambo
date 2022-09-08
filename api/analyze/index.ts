a54b3998fbf51a70e2fbb03c6a926ba7b5f997d2
Bea Stollnitz <bea@stollnitz.com>
2022-09-08 08:44:47 -0700

    Use node's built-in https request

--- api/analyze/index.ts
+++ api/analyze/index.ts
@@ -1,24 +1,46 @@
-import fetch from "node-fetch";
+import { request } from "https";
 import { RestError } from "@azure/core-http";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const inferenceApi = process.env["INFERENCE_ENDPOINT"] || '';
 const inferenceKey = process.env["INFERENCE_KEY"] || '';
 
+function httpsPost({body, ...options}) {
+    return new Promise<Buffer>((resolve, reject) => {
+        const req = request({
+            method: 'POST',
+            ...options,
+        }, res => {
+            const chunks = [];
+            res.on('data', data => chunks.push(data))
+            res.on('end', () => {
+                const resBody = Buffer.concat(chunks);
+                resolve(resBody)
+            })
+        })
+        req.on('error', reject);
+        if (body) {
+            req.write(body);
+        }
+        req.end();
+    })
+}
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     try {
-        // const response = await fetch(inferenceApi, {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //         Authorization: `Bearer ${inferenceKey}`,
-        //     },
-        //     body: req.rawBody
-        // });
-        // const text = await response.text()
+        const url = new URL(inferenceApi);
+        const response = await httpsPost({
+            host: url.host,
+            path: url.pathname,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${inferenceKey}`,
+            },
+            body: req.rawBody
+        });
         context.res = {
             status: 200,
-            body: req.rawBody
+            body: response
         };
     } catch (error) {
         const e = error as RestError;
